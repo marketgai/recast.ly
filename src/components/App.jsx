dbfeee12a7ee9c588db35e1828343fe1b6a3de45
Marketgai <marcusalee@gmail.com>
2020-10-20 01:01:37 -0700

    working on video list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@
 import exampleVideoData from './data/exampleVideoData.js';
 import Search from './search.js';
 import VideoList from './VideoList.js';
-import VideoListEntry from '.VideoListEntry.js';
+import VideoListEntry from './VideoListEntry.js';
 
 var App = () => (
   <div>
@@ -25,9 +25,9 @@ var App = () => (
         </div>
       </div>
       <div className="col-md-5">
-        <VideoList videos={exampleVideoData} />
         <div>
           <h5>
+            <VideoList videos={exampleVideoData} />
             <em>videoList</em> view goes here
           </h5>
         </div>
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,20 @@
+import VideoListEntry from './VideoListEntry.js';
+
 var VideoList = (props) => (
   <div className="video-list">
-    {console.log(props)}
     <div>
-      <VideoListEntry video={props.videos[0]} />
       <h5>
-        <em>videoListEntry</em> view goes here
+        <VideoListEntry videos={props.videos} />
+        {console.log(props.videos)}
+      </h5>
+      {/* <h5>
+        <div>{props.videos.map((video) =>
+          <VideoListEntry video={video} />)}</div>
+      </h5> */}
+    </div>
+    {/* <div>
+      <h5>
+        <em>videoListEntry</em> view goes here<VideoListEntry video={props.videos[0]} />
       </h5>
     </div>
     <div>
@@ -21,12 +31,7 @@ var VideoList = (props) => (
       <h5>
         <em>videoListEntry</em> view goes here
       </h5>
-    </div>
-    <div>
-      <h5>
-        <em>videoListEntry</em> view goes here
-      </h5>
-    </div>
+    </div> */}
   </div>
 );
 
