b28bf985f32434fb507251116779a6757bd4f448
Marketgai <marcusalee@gmail.com>
2020-10-19 23:35:43 -0700

    worked on passing data to videoentrylist

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,36 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+import exampleVideoData from './data/exampleVideoData.js';
+import Search from './search.js';
+import VideoList from './VideoList.js';
+import VideoListEntry from '.VideoListEntry.js';
+
 var App = () => (
   <div>
     <nav className="navbar">
       <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+        <div>
+          <h5>
+            <em>search</em> view goes here
+          </h5>
+        </div>
       </div>
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        <div>
+          <h5>
+            <em>videoPlayer</em> view goes here
+          </h5>
+        </div>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        <VideoList videos={exampleVideoData} />
+        <div>
+          <h5>
+            <em>videoList</em> view goes here
+          </h5>
+        </div>
       </div>
     </div>
   </div>
--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,11 +1,15 @@
-var VideoListEntry = () => (
+// use props as the param
+// update the videolistentry with the dynamic syntax
+
+var VideoListEntry = (props) => (
   <div className="video-list-entry media">
+    {console.log(props)}
     <div className="media-left media-middle">
-      <img className="media-object" src="https://i.ytimg.com/vi/1w8Z0UOXVaY/default.jpg" alt="" />
+      <img className="media-object" src={props.video.snippet.thumbnails.default.url} alt="" />
     </div>
     <div className="media-body">
-      <div className="video-list-entry-title">Video Title</div>
-      <div className="video-list-entry-detail">Video Description</div>
+      <div className="video-list-entry-title">{props.video.snippet.Title}</div>
+      <div className="video-list-entry-detail">{props.video.snippet.Description}</div>
     </div>
   </div>
 );
@@ -13,7 +17,7 @@ var VideoListEntry = () => (
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
 VideoListEntry.propTypes = {
-  video: PropTypes.object.isRequired
+  video : PropTypes.object.isRequired
 };
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
